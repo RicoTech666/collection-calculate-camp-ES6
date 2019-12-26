bb1b81cf360edca31073d7987e81c536f5476444
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-26 18:19:52 +0800

    [Jiachen] feat: double dimensional array to one 1 is finished

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,17 @@
-'use strict';
+"use strict";
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  var oneDimArr = [];
+  for (var i = 0; i < collection.length; i++) {
+    if ("number" === typeof collection[i]) {
+      oneDimArr.push(collection[i]);
+    } else if ("object" === typeof collection[i]) {
+      for (var j = 0; j < collection[i].length; j++) {
+        oneDimArr.push(collection[i][j]);
+      }
+    }
+  }
+  return oneDimArr;
 }
 
 module.exports = double_to_one;
