dd602ca031d707ed829a0f3f5928dbcf4c0841db
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-26 21:43:00 +0800

    [Jiachen] feat: map the array to its four times plus one or three times

--- main/map/map_to_three_multiples.js
+++ main/map/map_to_three_multiples.js
@@ -1,6 +1,8 @@
-'use strict';
-var map_to_three_multiples = function(collections){
-  return [3,9,15,12,27];
+"use strict";
+var map_to_three_multiples = function(collections) {
+  return collections.map(function(elem) {
+    return 3 * elem;
+  });
 };
 
 module.exports = map_to_three_multiples;
