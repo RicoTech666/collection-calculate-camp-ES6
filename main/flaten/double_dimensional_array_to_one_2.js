6c256e9af94cc5bb77efb0168d942686dfd0ba37
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-26 21:33:02 +0800

    [Jiachen] feat: double dimensional array to one dimensional array, and eliminate the repeated occurance in the one dimensional array

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,27 @@
-'use strict';
+"use strict";
 
 function double_to_one(collection) {
+  var oneDimArr = [];
+  for (var i = 0; i < collection.length; i++) {
+    if ("number" === typeof collection[i]) {
+      oneDimArr.push(collection[i]);
+    } else if ("object" === typeof collection[i]) {
+      for (var j = 0; j < collection[i].length; j++) {
+        oneDimArr.push(collection[i][j]);
+      }
+    }
+  }
+  return eliminateRepetition(oneDimArr);
+}
 
-  //在这里写入代码
+function eliminateRepetition(arr) {
+  var arrNoRepetition = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (-1 === arrNoRepetition.indexOf(arr[i])) {
+      arrNoRepetition.push(arr[i]);
+    }
+  }
+  return arrNoRepetition;
 }
 
 module.exports = double_to_one;
