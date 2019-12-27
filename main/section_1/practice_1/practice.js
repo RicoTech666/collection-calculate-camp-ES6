8402c822c55d41dfa22088dc6c1b209f1e445fea
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-27 19:54:28 +0800

    [Jiachen] feat: collect the same elements from another array

--- main/section_1/practice_1/practice.js
+++ main/section_1/practice_1/practice.js
@@ -1,5 +1,11 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  var theSameElems = [];
+  for(var i = 0;i<collection_a.length;i++) {
+    if(-1 != collection_b.indexOf(collection_a[i])) {
+      theSameElems.push(collection_a[i]);
+    }
+  }
+  return theSameElems;
 }
 
 module.exports = collect_same_elements;
