44b15a6073abf7667461c5fcbbccb5a98823f698
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-27 20:23:12 +0800

    [Jiachen] feat: select the same elements from the other object's 'value' attribute

--- main/section_1/practice_3/practice.js
+++ main/section_1/practice_3/practice.js
@@ -1,5 +1,20 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  var collectionB = getElemsByAttributeName(object_b, "value");
+  return collectSameElemsOneDim(collection_a,collectionB);
+}
+
+function getElemsByAttributeName(collectionObj, attrStr) {
+  return collectionObj[attrStr];
+}
+
+function collectSameElemsOneDim(collection_a, collection_b) {
+  var theSameElems = [];
+  for (var i = 0; i < collection_a.length; i++) {
+    if (-1 != collection_b.indexOf(collection_a[i])) {
+      theSameElems.push(collection_a[i]);
+    }
+  }
+  return theSameElems;
 }
 
 module.exports = collect_same_elements;
