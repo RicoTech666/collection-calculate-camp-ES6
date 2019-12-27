3bc5b074d457cf73aa5ac5dacbf46792b2aa460f
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-27 21:02:13 +0800

    [Jiachen] feat: count the same elements

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,25 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  var countedObjFromCollection = countArrToObj(collection);
+  return countObjToObjArray(countedObjFromCollection);
+}
+
+function countArrToObj(arr) {
+  return arr.reduce(function(allElems, elem) {
+    if (elem in allElems) {
+      allElems[elem]++;
+    } else {
+      allElems[elem] = 1;
+    }
+    return allElems;
+  }, {});
+}
+
+function countObjToObjArray(countedObj) {
+  var objArray = [];
+  for (var key in countedObj) {
+    objArray.push({'key':key, 'count':countedObj[key]});
+  }
+  return objArray;
 }
 
 module.exports = count_same_elements;
