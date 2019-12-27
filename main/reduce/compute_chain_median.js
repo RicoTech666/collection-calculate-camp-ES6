5ac321c73e69768d07da32c1f01cb387a96890ce
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-27 11:26:20 +0800

    [Jiachen] feat: compuete median and chain median of an array

--- main/reduce/compute_chain_median.js
+++ main/reduce/compute_chain_median.js
@@ -1,7 +1,32 @@
-'use strict';
+"use strict";
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  var arr = getArrayFromChain(collection);
+  var sortedArr = sortArrAsc(arr);
+  return getMedianFromArr(sortedArr);
+}
+
+function getArrayFromChain(strChain) {
+  var arr = strChain.split("->").map(Number);
+  return arr;
+}
+
+function sortArrAsc(arrUnsorted) {
+  var arrSorted = arrUnsorted.sort(function(leftNum, rightNum) {
+    return rightNum - leftNum;
+  });
+  return arrSorted;
+}
+
+function getMedianFromArr(arrSorted) {
+  var arrLen = arrSorted.length;
+  var medianOfArr;
+  if (0 === arrLen % 2) {
+    medianOfArr = (arrSorted[arrLen / 2] + arrSorted[arrLen / 2 - 1]) / 2;
+  } else {
+    medianOfArr = arrSorted[(arrLen - 1) / 2];
+  }
+  return medianOfArr;
 }
 
 module.exports = compute_chain_median;
--- main/reduce/compute_median.js
+++ main/reduce/compute_median.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 function compute_median(collection) {
-  //在这里写入代码
+  var sortedArr = sortArrAsc(collection);
+  return getMedianFromArr(sortedArr);
+}
+function sortArrAsc(arrUnsorted) {
+  var arrSorted = arrUnsorted.sort(function(leftNum, rightNum) {
+    return rightNum - leftNum;
+  });
+  return arrSorted;
+}
+
+function getMedianFromArr(arrSorted) {
+  var arrLen = arrSorted.length;
+  var medianOfArr;
+  if (0 === arrLen % 2) {
+    medianOfArr = (arrSorted[arrLen / 2] + arrSorted[arrLen / 2 - 1]) / 2;
+  } else {
+    medianOfArr = arrSorted[(arrLen - 1) / 2];
+  }
+  return medianOfArr;
 }
 
 module.exports = compute_median;
-
-
