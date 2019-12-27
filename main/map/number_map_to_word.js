fc988ac956d98f752380bbd1beaecaf6484a7fdc
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-27 09:54:09 +0800

    [Jiachen] feat: map number to word

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,10 @@
-'use strict';
-var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+"use strict";
+var number_map_to_word = function(collection) {
+  return collection.map(function(elem) {
+    if (elem <= 26) {
+     return String.fromCharCode(elem + 96);
+    }
+  });
 };
 
 module.exports = number_map_to_word;
