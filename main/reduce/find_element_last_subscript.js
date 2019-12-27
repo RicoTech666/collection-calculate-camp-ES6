20a23a142bcde7119ed59e32ccc37d840bfa52c4
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-27 11:43:17 +0800

    [Jiachen] feat: find element last and first subscript

--- main/reduce/find_element_last_subscript.js
+++ main/reduce/find_element_last_subscript.js
@@ -1,7 +1,17 @@
-'use strict';
+"use strict";
 
 function calculate_elements_sum(collection, element) {
-  //在这里写入代码
+  var matchedSubArr = getMathcedSubArr(collection, element);
+  return matchedSubArr[matchedSubArr.length - 1];
 }
 
+function getMathcedSubArr(collection, element) {
+  var matchedSubArr = collection.reduce(function(matchedSub, arrElem, index) {
+    if (arrElem === element) {
+      matchedSub.push(index);
+    }
+    return matchedSub;
+  }, []);
+  return matchedSubArr;
+}
 module.exports = calculate_elements_sum;
