60b758ca51bee084cfa6890666a16aef3aea8f7f
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-27 13:42:18 +0800

    [Jiachen] feat: split to zero

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -1,7 +1,48 @@
-'use strict';
+"use strict";
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  var isZero = canBeSplitedToZero(number, interval);
+  return generateSplitArr(number, interval, isZero);
+}
+
+function canBeSplitedToZero(number, interval) {
+  var numberInt = toInt(number);
+  var intervalInt = toInt(interval);
+  if (0 === numberInt % intervalInt) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function generateSplitArr(number, interval, canBeSplitedToZero) {
+  var splitedArr = [];
+  var numberInt = toInt(number);
+  var intervalInt = toInt(interval);
+  if (canBeSplitedToZero) {
+    while (numberInt >= 0) {
+      splitedArr.push(toFloat(numberInt));
+      numberInt -= intervalInt;
+    }
+  } else {
+    while (numberInt > -intervalInt) {
+      splitedArr.push(toFloat(numberInt));
+      numberInt -= intervalInt;
+    }
+  }
+  return splitedArr;
+}
+
+function toInt(num) {
+  return parseInt(num.toString().split(".")[1]);
+}
+
+function toFloat(num) {
+  if (num >= 0) {
+    return parseFloat("0." + num.toString());
+  } else {
+    return parseFloat("-0." + (-num).toString());
+  }
 }
 
 module.exports = spilt_to_zero;
