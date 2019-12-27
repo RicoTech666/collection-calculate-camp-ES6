3debb65045800f9e3c0c422e33c8069dad2e2c1e
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-27 10:00:48 +0800

    [Jiachen] feat: number map to word over 26

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -2,7 +2,7 @@
 var number_map_to_word = function(collection) {
   return collection.map(function(elem) {
     if (elem <= 26) {
-     return String.fromCharCode(elem + 96);
+      return String.fromCharCode(elem + 96);
     }
   });
 };
--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,13 @@
-'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+"use strict";
+var number_map_to_word_over_26 = function(collection) {
+  var letterLen = 26;
+  return collection.map(function(elem) {
+    if (elem <= letterLen) {
+      return String.fromCharCode(elem + 96);
+    } else if (elem <= letterLen * 2) {
+      return "a" + String.fromCharCode(elem + 96 - letterLen);
+    }
+  });
 };
 
 module.exports = number_map_to_word_over_26;
