7a30ccabb6c1ebe753b66bb44eb205a9b3603256
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-27 10:22:44 +0800

    [Jiachen] feat: sort in ascending order

--- main/map/rank_asc.js
+++ main/map/rank_asc.js
@@ -1,6 +1,8 @@
-'use strict';
-var rank_asc = function(collection){
-  return [6,5,4,3,2];
+"use strict";
+var rank_asc = function(collection) {
+  return collection.sort(function(leftNum,rightNum) {
+    return rightNum-leftNum;
+  });
 };
 
 module.exports = rank_asc;
--- main/map/rank_desc.js
+++ main/map/rank_desc.js
@@ -1,6 +1,8 @@
 'use strict';
 var rank_desc = function(collection){
-  return [2,3,4,5,6];
+  return collection.sort(function(leftNum,rightNum) {
+    return rightNum-leftNum;
+  });
 };
 
 module.exports = rank_desc;
