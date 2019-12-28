626d47adffa6e088e8a701c5a3df8b331ad92cdc
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-28 11:24:57 +0800

    [Jiachen] feat: calculate the number of each elements from an array

--- main/section_2/practice_2/practice.js
+++ main/section_2/practice_2/practice.js
@@ -1,5 +1,41 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  var countedObjFromCollection = countArrToObj(collection);
+  var objArrayDashTransfered = transferDashNumberToValue(
+    countedObjFromCollection
+  );
+  return countObjToObjArray(objArrayDashTransfered);
+}
+
+function countArrToObj(arr) {
+  return arr.reduce(function(allElems, elem) {
+    if (elem in allElems) {
+      allElems[elem]++;
+    } else {
+      allElems[elem] = 1;
+    }
+    return allElems;
+  }, {});
+}
+
+function transferDashNumberToValue(objNeedToBeTransfered) {
+  var arrObj = objNeedToBeTransfered;
+  for (var key in arrObj) {
+    if (-1 !== key.indexOf("-")) {
+      var newKey = key.split("-")[0];
+      var newValue = key.split("-")[1];
+      delete arrObj[key];
+      arrObj[newKey] = parseInt(newValue);
+    }
+  }
+  return arrObj;
+}
+
+function countObjToObjArray(countedObj) {
+  var objArray = [];
+  for (var key in countedObj) {
+    objArray.push({ key: key, count: countedObj[key] });
+  }
+  return objArray;
 }
 
 module.exports = count_same_elements;
