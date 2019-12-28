eeb92ee806100cbe5532f22e69ece8bf52b464a8
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-28 21:12:32 +0800

    [Jiachen] feat: create updated collection 3

--- main/section_3/practice_3/practice.js
+++ main/section_3/practice_3/practice.js
@@ -1,12 +1,37 @@
 function create_updated_collection(collection_a, object_b) {
-  for (var i = 0; i < collection_a.length; i++) {
+  var collectionACounted = count_same_elements(collection_a);
+  for (var i = 0; i < collectionACounted.length; i++) {
     for (var j = 0; j < object_b.value.length; j++) {
-      if (collection_a[i].key === object_b.value[j]) {
-        collection_a[i].count -= collection_a[i].count / 3;
+      if (collectionACounted[i].key === object_b.value[j]) {
+        collectionACounted[i].count -= Math.floor(collectionACounted[i].count / 3);
       }
     }
   }
-  return collection_a;
+  return collectionACounted;
+}
+
+function count_same_elements(collection) {
+  var countedObjFromCollection = countArrToObj(collection);
+  return countObjToObjArray(countedObjFromCollection);
+}
+
+function countArrToObj(arr) {
+  return arr.reduce(function(allElems, elem) {
+    if (elem in allElems) {
+      allElems[elem]++;
+    } else {
+      allElems[elem] = 1;
+    }
+    return allElems;
+  }, {});
+}
+
+function countObjToObjArray(countedObj) {
+  var objArray = [];
+  for (var key in countedObj) {
+    objArray.push({'key':key, 'count':countedObj[key]});
+  }
+  return objArray;
 }
 
 module.exports = create_updated_collection;
