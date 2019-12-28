a4ccbad5648248130047eb675f1d4403e26a5698
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-28 21:14:03 +0800

    [Jiachen] feat: create updated collection 4

--- main/section_3/practice_4/practice.js
+++ main/section_3/practice_4/practice.js
@@ -1,5 +1,52 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  var collectionACounted = count_same_elements(collection_a);
+  for (var i = 0; i < collectionACounted.length; i++) {
+    for (var j = 0; j < object_b.value.length; j++) {
+      if (collectionACounted[i].key === object_b.value[j]) {
+        collectionACounted[i].count -= Math.floor(collectionACounted[i].count / 3);
+      }
+    }
+  }
+  return collectionACounted;
 }
 
+function count_same_elements(collection) {
+  var countedObjFromCollection = countArrToObj(collection);
+  var objArrayDashTransfered = transferDashNumberToValue(
+    countedObjFromCollection
+  );
+  return countObjToObjArray(objArrayDashTransfered);
+}
+
+function countArrToObj(arr) {
+  return arr.reduce(function(allElems, elem) {
+    if (elem in allElems) {
+      allElems[elem]++;
+    } else {
+      allElems[elem] = 1;
+    }
+    return allElems;
+  }, {});
+}
+
+function transferDashNumberToValue(objNeedToBeTransfered) {
+  var arrObj = objNeedToBeTransfered;
+  for (var key in arrObj) {
+    if (-1 !== key.indexOf("-")) {
+      var newKey = key.split("-")[0];
+      var newValue = key.split("-")[1];
+      delete arrObj[key];
+      arrObj[newKey] = parseInt(newValue);
+    }
+  }
+  return arrObj;
+}
+
+function countObjToObjArray(countedObj) {
+  var objArray = [];
+  for (var key in countedObj) {
+    objArray.push({ key: key, count: countedObj[key] });
+  }
+  return objArray;
+}
 module.exports = create_updated_collection;
