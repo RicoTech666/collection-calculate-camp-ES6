e6d997afbdf7d2c9206358c95d4b423c7c3d06cd
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-29 21:01:23 +0800

    [Jiachen] feat: rank by two large one small

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,25 @@
-'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+"use strict";
+function rank_by_two_large_one_small(collection) {
+  var ascArr = rankAsc(collection);
+  var index = 0;
+  console.log(ascArr);
+  while (index < ascArr.length - 2) {
+    swapElemsInArray(ascArr, index, index + 1);
+    swapElemsInArray(ascArr, index + 1, index + 2);
+    index += 3;
+  }
+  return ascArr;
 }
+
+function rankAsc(collection) {
+  return collection.sort(function(leftNum, rightNum) {
+    return leftNum - rightNum;
+  });
+}
+
+function swapElemsInArray(arr, index1, index2) {
+  arr[index1] = arr.splice(index2, 1, arr[index1])[0];
+  return arr;
+}
+
 module.exports = rank_by_two_large_one_small;
