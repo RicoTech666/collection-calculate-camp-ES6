5394b077b1278f29669d2c284047a06b05ae566a
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-29 12:40:45 +0800

    [Jiachen] feat: calculate even index sub-array's median

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,29 @@
-'use strict';
-var calculate_median = function(collection){
-
+"use strict";
+var calculate_median = function(collection) {
+  var evenElemsArr = getEvenElems(collection);
+  return getMedianFromArr(evenElemsArr);
 };
+
+function getEvenElems(originalArr) {
+  var evenArr = [];
+  for (let index = 0; index < originalArr.length; index++) {
+    const elem = originalArr[index];
+    if (0 !== index % 2) {
+      evenArr.push(elem);
+    }
+  }
+  return evenArr;
+}
+
+function getMedianFromArr(arrSorted) {
+  var arrLen = arrSorted.length;
+  var medianOfArr;
+  if (0 === arrLen % 2) {
+    medianOfArr = (arrSorted[arrLen / 2] + arrSorted[arrLen / 2 - 1]) / 2;
+  } else {
+    medianOfArr = arrSorted[(arrLen - 1) / 2];
+  }
+  return medianOfArr;
+}
+
 module.exports = calculate_median;
