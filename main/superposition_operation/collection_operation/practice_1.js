49590ef90aff4099e0f3cb35043cdd06871eb487
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-29 11:19:28 +0800

    [Jiachen] refactor: delete console log

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -2,12 +2,9 @@
 
 function hybrid_operation(collection) {
   return collection.reduce(function(calculated, collectionElem, index) {
-    if (0 !== index) {
-      calculated += collectionElem * 3 + 2;
-    }
+    calculated += collectionElem * 3 + 2;
     return calculated;
-  }, collection[0] * 3 + 2);
+  }, 0);
 }
 
-console.log(hybrid_operation([1]));
 module.exports = hybrid_operation;
