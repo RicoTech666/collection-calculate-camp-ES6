13720834f06bee55d096090cb0e63bf6a8e1c855
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-29 11:38:46 +0800

    [Jiachen] feat: calculate the average of odd elements in an array

--- main/superposition_operation/interval_operation/average_uneven.js
+++ main/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,15 @@
-'use strict';
+"use strict";
 
 function average_uneven(collection) {
-
-  //在这里写入代码
+  var oddCount = 0;
+  var oddSum = 0;
+  for (let i = 0; i < collection.length; i++) {
+    if (0 !== collection[i] % 2) {
+      oddSum += collection[i];
+      oddCount++;
+    }
+  }
+  return oddSum / oddCount;
 }
 
 module.exports = average_uneven;
