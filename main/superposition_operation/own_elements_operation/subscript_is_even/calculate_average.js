394b86649eef28f25a81084641a37c388b51a2d0
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-29 12:30:39 +0800

    [Jiachen] feat: calculate average when subscript is even

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,13 @@
-'use strict';
-var calculate_average = function(collection){
-
+"use strict";
+var calculate_average = function(collection) {
+  var sum = 0;
+  var count = 0;
+  for (let i = 0; i < collection.length; i++) {
+    if (0 !== i % 2) {
+      sum += collection[i];
+      count++;
+    }
+  }
+  return sum / count;
 };
 module.exports = calculate_average;
