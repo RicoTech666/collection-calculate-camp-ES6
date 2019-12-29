e55a22319e1c4ccc84c9c697234895ecc8caf2f3
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-29 20:38:58 +0800

    [Jiachen] feat: even ascending and odd descending

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,47 @@
-'use strict';
-var even_asc_odd_desc = function(collection){
-
+"use strict";
+var even_asc_odd_desc = function(collection) {
+  var evenSubAsc = rankAsc(getEvenElems(collection));
+  console.log(evenSubAsc);
+  var oddSubDesc = rankDesc(getOddElems(collection));
+  var evenAscOddDesc = evenSubAsc;
+  while (0 !== oddSubDesc.length) {
+    evenAscOddDesc.push(oddSubDesc.shift());
+  }
+  return evenAscOddDesc;
 };
+
+function getEvenElems(arr) {
+  var evenArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const elem = arr[i];
+    if (0 === elem % 2) {
+      evenArr.push(elem);
+    }
+  }
+  return evenArr;
+}
+
+function getOddElems(arr) {
+  var evenArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const elem = arr[i];
+    if (0 !== elem % 2) {
+      evenArr.push(elem);
+    }
+  }
+  return evenArr;
+}
+
+function rankAsc(collection) {
+  return collection.sort(function(leftNum, rightNum) {
+    return leftNum - rightNum;
+  });
+}
+
+function rankDesc(collection) {
+  return collection.sort(function(leftNum, rightNum) {
+    return rightNum - leftNum;
+  });
+}
+
 module.exports = even_asc_odd_desc;
--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,45 @@
-'use strict';
-var single_element = function(collection){
-
+"use strict";
+var single_element = function(collection) {
+  var eventhArr = getEventhElems(collection);
+  var reducedObj = getReducedObj(eventhArr);
+  var singleElements = findDistinctElemsFromReducedObj(reducedObj);
+  return singleElements;
 };
+var collection_a = [1, 2, 3, 2, 5, 6, 21, 43, 12, 5];
+console.log(getEventhElems(collection_a));
+function getEventhElems(originalArr) {
+  var evenIndexArr = [];
+  for (let index = 0; index < originalArr.length; index++) {
+    const elem = originalArr[index];
+    if (0 !== index % 2) {
+      evenIndexArr.push(elem);
+    }
+  }
+  return evenIndexArr;
+}
+
+function getReducedObj(arr) {
+  return arr.reduce((reducedArr, eachElem) => {
+    if (eachElem in reducedArr) {
+      reducedArr[eachElem]++;
+    } else {
+      reducedArr[eachElem] = 1;
+    }
+    return reducedArr;
+  }, {});
+}
+
+function findDistinctElemsFromReducedObj(objReduced) {
+  var arrDinstinct = [];
+  var objKeysArr = Object.keys(objReduced);
+  for (let i = 0; i < objKeysArr.length; i++) {
+    var currentKey = objKeysArr[i];
+    var currentValue = objReduced[currentKey];
+    if (1 === currentValue) {
+      arrDinstinct.push(currentKey);
+    }
+  }
+  return arrDinstinct;
+}
+
 module.exports = single_element;
