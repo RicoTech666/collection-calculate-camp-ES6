312de9ee7d08a7b964d3c40b9bc5950af69b567d
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-29 11:16:31 +0800

    [Jiachen] feat: odd element * 3 + 2

--- main/superposition_operation/collection_operation/practice_2.js
+++ main/superposition_operation/collection_operation/practice_2.js
@@ -1,9 +1,13 @@
-'use strict';
+"use strict";
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  var result = [];
+  for (var i = 0; i < collection.length; i++) {
+    if (0 !== collection[i] % 2) {
+      result.push(collection[i] * 3 + 2);
+    }
+  }
+  return result;
 }
 
 module.exports = hybrid_operation_to_uneven;
-
