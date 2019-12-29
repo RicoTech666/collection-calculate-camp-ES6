5f0e055282b4eea3750b17b6aa77c09a4ae0d28d
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-29 11:12:13 +0800

    [Jiachne] feat: array element mulitiply with 3 and plus 2, then calculate the sum of them

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -1,9 +1,13 @@
-'use strict';
+"use strict";
 
 function hybrid_operation(collection) {
-
-  //在这里写入代码
+  return collection.reduce(function(calculated, collectionElem, index) {
+    if (0 !== index) {
+      calculated += collectionElem * 3 + 2;
+    }
+    return calculated;
+  }, collection[0] * 3 + 2);
 }
 
+console.log(hybrid_operation([1]));
 module.exports = hybrid_operation;
-
