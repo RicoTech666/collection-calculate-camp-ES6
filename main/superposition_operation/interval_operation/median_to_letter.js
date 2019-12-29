8e60dc4f12d38c007a698e01e63dc2f456b9f697
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-29 12:27:03 +0800

    [Jiachen] feat: median to letter

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,40 @@
-'use strict';
+"use strict";
 
 function median_to_letter(collection) {
+  var medianVal = compute_median(collection);
+  return mapLetterOver26(medianVal);
+}
 
-  //在这里写入代码
+function compute_median(collection) {
+  var sortedArr = sortArrAsc(collection);
+  return getMedianFromArr(sortedArr);
+}
+
+function sortArrAsc(arrUnsorted) {
+  var arrSorted = arrUnsorted.sort(function(leftNum, rightNum) {
+    return rightNum - leftNum;
+  });
+  return arrSorted;
+}
+
+function getMedianFromArr(arrSorted) {
+  var arrLen = arrSorted.length;
+  var medianOfArr;
+  if (0 === arrLen % 2) {
+    medianOfArr = (arrSorted[arrLen / 2] + arrSorted[arrLen / 2 - 1]) / 2;
+  } else {
+    medianOfArr = arrSorted[(arrLen - 1) / 2];
+  }
+  return Math.ceil(medianOfArr);
+}
+
+function mapLetterOver26(medianVal) {
+  var letterLen = 26;
+  if (medianVal <= letterLen) {
+    return String.fromCharCode(elem + 96);
+  } else if (medianVal <= letterLen * 2) {
+    return "a" + String.fromCharCode(medianVal + 96 - letterLen);
+  }
 }
 
 module.exports = median_to_letter;
