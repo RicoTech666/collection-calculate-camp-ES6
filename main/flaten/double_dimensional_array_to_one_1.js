d8c4c346c3f233054fbd92d59d7bba86e7af24ca
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-07 21:35:34 +0800

    [Jiachen] refactor: refactor flaten functions by ES6

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,17 +1,7 @@
 "use strict";
 
 function double_to_one(collection) {
-  var oneDimArr = [];
-  for (var i = 0; i < collection.length; i++) {
-    if ("number" === typeof collection[i]) {
-      oneDimArr.push(collection[i]);
-    } else if ("object" === typeof collection[i]) {
-      for (var j = 0; j < collection[i].length; j++) {
-        oneDimArr.push(collection[i][j]);
-      }
-    }
-  }
-  return oneDimArr;
+  return collection.flat();
 }
 
 module.exports = double_to_one;
--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,27 +1,7 @@
 "use strict";
 
 function double_to_one(collection) {
-  var oneDimArr = [];
-  for (var i = 0; i < collection.length; i++) {
-    if ("number" === typeof collection[i]) {
-      oneDimArr.push(collection[i]);
-    } else if ("object" === typeof collection[i]) {
-      for (var j = 0; j < collection[i].length; j++) {
-        oneDimArr.push(collection[i][j]);
-      }
-    }
-  }
-  return eliminateRepetition(oneDimArr);
-}
-
-function eliminateRepetition(arr) {
-  var arrNoRepetition = [];
-  for (var i = 0; i < arr.length; i++) {
-    if (-1 === arrNoRepetition.indexOf(arr[i])) {
-      arrNoRepetition.push(arr[i]);
-    }
-  }
-  return arrNoRepetition;
+  return [...new Set(collection.flat())];
 }
 
 module.exports = double_to_one;
